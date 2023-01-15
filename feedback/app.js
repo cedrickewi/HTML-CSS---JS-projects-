7826d00c531409b2e5063bf4527b6d47ca846d7f
cedrickewi <cedrickewi@gmail.com>
2023-01-15 13:18:40 +0100

    feadback app finished

--- feedback/app.js
+++ feedback/app.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+const btn = document.getElementById("btn")
+const feadbacks = document.querySelector(".feadbacks")
+const result = document.querySelector(".result")
+const feadbacksImo = document.querySelectorAll(".feadback")
+const response = document.querySelector(".response")
+
+console.log(response);
+
+btn.addEventListener("click", ()=>{
+    feadbacks.style.display = "none"
+    result.style.display = "flex"
+    feadbacksImo.forEach(feadbackWord => {
+        if(feadbackWord.classList.contains("active")){
+           let yes =  feadbackWord.childNodes[3].textContent;
+           response.textContent = yes
+        }
+    })
+})
+
+feadbacksImo.forEach(feadback => {
+    feadback.addEventListener("click", () => {
+        feadbacksImo.forEach(feadbackActive=>{
+            feadbackActive.classList.remove("active")
+        })
+        feadback.classList.add("active")
+        const x = feadback.childNodes[3].textContent;
+        console.log(x);
+    })
+})
+
